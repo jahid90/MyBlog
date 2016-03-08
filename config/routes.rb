20b7064906a90afb7f712c8f61589d0ebbd03f8a
Jahid <s.jahid.90@gmail.com>
2016-03-08 22:13:27 +0530

    add landing page for guest user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
+  authenticated :user do
+    root 'posts#index', as: "authenticated_root"
+  end
+
+  root "pages#home"
+
   devise_for :users
-  root 'posts#index'
 
   resources :posts do
     resources :comments, except: :index
