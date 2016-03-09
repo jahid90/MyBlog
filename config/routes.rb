a7efdb0eabe8b5925bd476bd9548f7125eadd09e
Jahid <s.jahid.90@gmail.com>
2016-03-09 20:43:33 +0530

    add feature to list all posts on the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
   authenticated :user do
-    root 'posts#index', as: "authenticated_root"
+    root 'posts#index', as: 'authenticated_root'
   end
 
-  root "pages#home"
+  root 'pages#home'
+
+  get '/posts', to: 'pages#posts'
 
   devise_for :users
 
